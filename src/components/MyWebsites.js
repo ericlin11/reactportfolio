268d6a4760e7d635d274af2e65f6de08a8bd4eab
Eric Lin <eric.lin206@gmail.com>
2021-12-16 11:03:59 -0500

    new commit

--- src/components/MyWebsites.js
+++ src/components/MyWebsites.js
@@ -5,9 +5,9 @@
 const MyWebsites = [
     {
         id: 1,
-        title: "Fullstack Web Development Project",
-        link: "https://github.com/ericlin11/Fullstack-Web-Development-Class",
-        summary: "A full stack web project that is made up of HTML5, CSS3, JavaScript for front-end and PHP, SQL for backend. This website pulls data from a database server and shows the user the data table. The user can update, delete, or add new data to the database server directly from the website.",
+        title: "Keeper App",
+        link: "https://codesandbox.io/s/using-pre-built-react-components-forked-nn5qo",
+        summary: "My first react project. Allows user to take notes, add a title and content to the note. User can delete individual note when they are done with it.",
         src: "./images/KeeperApp.JPG"
     },
     {
@@ -15,14 +15,14 @@ const MyWebsites = [
         title: "Java JSP Website",
         link: "https://github.com/ericlin11/Java-JSP-Project",
         summary: "A dynamic web content created with JSP and JavaFX. This web application connects to a database server, reads data, and display it as a GUI. User can freely add, update, and delete data from the database.",
-        src: "./images/KeeperApp.JPG"
+
     },
     {
         id: 3,
-        title: "Keeper App",
-        link: "https://codesandbox.io/s/using-pre-built-react-components-forked-nn5qo",
-        summary: "My first react project. Allows user to take notes, add a title and content to the note. User can delete individual note when they are done with it.",
-        src: "./images/KeeperApp.JPG"
+        title: "Fullstack Web Development Project",
+        link: "https://github.com/ericlin11/Fullstack-Web-Development-Class",
+        summary: "A full stack web project that is made up of HTML5, CSS3, JavaScript for front-end and PHP, SQL for backend. This website pulls data from a database server and shows the user the data table. The user can update, delete, or add new data to the database server directly from the website.",
+
     },
 ]
 
